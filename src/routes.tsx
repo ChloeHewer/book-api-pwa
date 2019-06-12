2a18896023506c5e767928eeac6287ebab318d83
Chloe Hewer <chloehewer@yahoo.com>
2019-06-12 14:23:00 +0100

    routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,35 +3,11 @@ import { Route, Switch, Redirect } from "react-router";
 import GreekGod from "./containers/greekGodContainer/greekGod";
 import Quiz from "./containers/quiz/quiz";
 
-interface IQuizQuestions {
-  category: string;
-  type: string;
-  difficulty: string;
-  question: string;
-  correct_answer: string;
-  incorrect_answers: string[];
-}
-
 export interface IProps {}
 
-export interface IState {
-  quizQuestions: IQuizQuestions | null;
-}
+export interface IState {}
 
 class Routes extends React.Component<IProps, IState> {
-  public state = { quizQuestions: null };
-  componentDidMount() {
-    fetch(
-      "https://opentdb.com/api.php?amount=10&category=20&difficulty=easy&type=multiple"
-    )
-      .then(results => {
-        return results.json();
-      })
-      .then(data => {
-        this.setState({ quizQuestions: data.results });
-      });
-  }
-
   public render() {
     return (
       <Switch>
